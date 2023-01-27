d11cf9848c8a536a8b7bbf96beacd8026ba6a2bd
Priyosmita <priyosh08@gmail.com>
2023-01-27 11:54:52 +0530

    css

--- src/component/CharacterPage.js
+++ src/component/CharacterPage.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
+import Spinner from "./Spinner";
 
 const CharacterPage = () => {
   const [data, setData] = useState([]);
@@ -21,20 +22,31 @@ const CharacterPage = () => {
       <h1 className="text-yellow-400 text-center font-bold text-4xl">
         Character Details
       </h1>
-      <div className="mt-20 w-full h-full flex flex-col items-center md:items-start md:flex-row md:justify-center">
-        <div className="md:w-[50%] flex justify-center md:justify-start w-full h-full">
-          <img
-            src={data.imageUrl}
-            alt=""
-            className="w-[80%] h-[75vh] object-cover rounded-lg"
-          />
-        </div>
-        <div className="text-white mt-5 md:mt-32 " >
+      {!loading && (
+        <div className="mt-20 w-full h-full flex flex-col items-center md:items-start md:flex-row md:justify-center">
+          <div className="md:w-[50%] flex justify-center md:justify-start w-full h-full">
+            <img
+              src={data.imageUrl}
+              alt=""
+              className="w-[80%] h-[75vh] object-cover rounded-lg"
+            />
+          </div>
+          <div className="text-white mt-5 md:mt-32 ">
             <p className="text-3xl py-2 font-bold">{data.fullName}</p>
-            <p className="text-xl py-1 text-center md:text-start">{data.family}</p>            
-            <p className="text-xl py-1 text-center md:text-start">{data.title}</p>              
+            <p className="text-xl py-1 text-center md:text-start">
+              {data.family}
+            </p>
+            <p className="text-xl py-1 text-center md:text-start">
+              {data.title}
+            </p>
+          </div>
         </div>
-      </div>
+      )}
+      {loading && (
+        <div className="w-full flex justify-center items-center h-screen">
+            <Spinner />
+        </div>
+      )}
     </div>
   );
 };
